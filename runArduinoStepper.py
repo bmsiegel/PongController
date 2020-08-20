e3932a621482fbc063a07952cd094e6e56979e13
Brady Siegel <bmsiegel@vt.edu>
2020-08-20 19:52:06 -0400

    update gitignore

--- runArduinoStepper.py
+++ runArduinoStepper.py
@@ -2,8 +2,7 @@ from ArduinoStepper import ArduinoStepper
 
 a = ArduinoStepper(200, 19, 13, 6, 5, 26)
 
-a.setSpeed(75)
+a.setSpeed(40)
 while 1:
-    input()
     a.step(int(input()))
 
--- runMotor.py
+++ runMotor.py
@@ -2,7 +2,7 @@ from MotorController import *
 
 GPIO.setmode(GPIO.BCM)
 
-m = MotorController()
+m = MotorController(1)
 
 m.enableMotor()
 m.readCtrl()
@@ -13,6 +13,7 @@ m.checkStatus()
 
 while 1:
     m.shoot(int(input('Enter speed:\t')))
+    time.sleep(1)
     m.sleep()
     input('Press Enter to Continue:')
     m.wake()
